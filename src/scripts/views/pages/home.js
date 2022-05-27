54d6336c11ff6cf683d8375ce4d816c8f92c4ad0
ASTRSM <69696478+ASTRSM@users.noreply.github.com>
2022-05-27 19:16:15 +0700

    Hero

--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -1,8 +1,17 @@
 const home = {
   render () {
     return `
-    <h1>Restlater</h1>
-    <img src="./images/logo-large.png" alt="Restlater Logo" height='400px'>
+    <div id='hero'>
+      <img src="./images/logo-large.png" alt="Restlater Logo" height='400px'>
+      <div id='right-hero'>
+        <h1>
+          Persiapkan diri anda dan keluarga untuk hal yang pasti akan datang ke kehidupan manusia.
+        </h1>
+        <a href='#blok'>
+          RESERVE NOW
+        </a>
+      </div>
+    </div>
     `
   }
 }
