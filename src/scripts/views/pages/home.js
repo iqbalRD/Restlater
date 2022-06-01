edc8a0491c60479a6298f3465779470d11d3b886
ASTRSM <69696478+ASTRSM@users.noreply.github.com>
2022-06-02 02:02:37 +0700

    Change the size of images into a static size

--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -6,7 +6,7 @@ const home = {
     document.title = 'Restlater | Reserve your own future'
     return `
     <section id='hero'>
-      <img src="./images/logo-large.png" alt="Restlater Logo" height='400px'>
+      <img src="./images/logo-large.png" alt="Restlater Logo" height='470px' width='420px'>
       <div id='right-hero'>
         <h1>
           Prepare yourself and your family for the things that will surely come to human life
@@ -22,22 +22,22 @@ const home = {
       </div>
       <div id='reason-content'>
         <div class='reason-item'>
-          <img src="./images/time.svg" alt="Time saver logo" height='100px'>
+          <img src="./images/time.svg" alt="Time saver logo" height='100px' width='100px'>
           <h3>TIME SAVER</h3>
           <p>Save your or your families time for having a slot</p>
         </div>
         <div class='reason-item'>
-          <img src="./images/search.svg" alt="Easy search logo" height='100px'>
+          <img src="./images/search.svg" alt="Easy search logo" height='100px' width='100px'>
           <h3>EASY SEARCH</h3>
           <p>Searching for a slot has never been this easy</p>
         </div>
         <div class='reason-item'>
-          <img src="./images/capacity.svg" alt="Time saver logo" height='100px'>
+          <img src="./images/capacity.svg" alt="Time saver logo" height='100px' width='100px'>
           <h3>SAFE PLAN</h3>
           <p>Never fear the depletion of slots on the public services</p>
         </div>
         <div class='reason-item'>
-          <img src="./images/safe.svg" alt="Time saver logo" height='100px'>
+          <img src="./images/safe.svg" alt="Time saver logo" height='100px' width='100px'>
           <h3>RELAX</h3>
           <p>Feel safe & sound when the time finally comes</p>
         </div>
