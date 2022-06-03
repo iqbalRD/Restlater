7301e81b20c5f964e4d87f8f393c562791d53e81
iqbal Rafiud Drajat <97111812+iqbalRD@users.noreply.github.com>
2022-06-03 15:31:58 +0700

    Add unfinished dashboard page
    * update test
    
    * Update
    
    * update
    
    * Dashboar (HTML + CSS) Belum Selesai Sepenuhnya
    
    * Dashboard nya udah dimasukkan ke page (masih 30%)

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,15 +1,19 @@
 import about from '../views/pages/about'
 import blok from '../views/pages/blok'
 import home from '../views/pages/home'
-import { payment } from '../views/pages/payment'
+import {
+  payment
+} from '../views/pages/payment'
 import profile from '../views/pages/profile'
+import dashboard from '../views/pages/dashboard'
 
 const routes = {
   '/': home, // default page
   '/blok/:id': blok,
   '/payment': payment,
   '/profile/:id': profile,
-  '/about': about
+  '/about': about,
+  '/dashboard': dashboard
 }
 
 export default routes
