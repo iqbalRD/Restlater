f58b4391349a931880a82752970fa4207862fd78
adwitiyan <alvino.nareswa@gmail.com>
2022-06-10 14:04:19 +0700

    fixing service worker plugin

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -14,6 +14,5 @@ $(function () {
   ;(async () => {
     await renderPage()
   })()
-
   swRegister()
 })
--- src/scripts/utils/sw-register.js
+++ src/scripts/utils/sw-register.js
@@ -4,6 +4,7 @@ const swRegister = async () => {
   if ('serviceWorker' in navigator) {
     const wb = new Workbox('/service-worker.js')
     wb.register()
+    return
   }
   console.log('Service worker not supported in this browser')
 }
