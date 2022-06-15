d1588586c39c027566e0fe112ef56fa8ea002bb1
ASTRSM <69696478+ASTRSM@users.noreply.github.com>
2022-06-15 23:51:06 +0700

    Fix Workbox Service Worker Function

--- src/scripts/utils/sw-register.js
+++ src/scripts/utils/sw-register.js
@@ -1,10 +1,19 @@
-import { Workbox } from 'workbox-window'
+// import { Workbox } from 'workbox-window'
 
 const swRegister = async () => {
+  // if ('serviceWorker' in navigator) {
+  //   const wb = new Workbox('/sw.js')
+  //   wb.register()
+  // }
+  // console.log('Service worker not supported in this browser')
   if ('serviceWorker' in navigator) {
-    const wb = new Workbox('/service-worker.js')
-    wb.register()
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('./service-worker.js').then(registration => {
+        console.log('SW registered: ', registration)
+      }).catch(registrationError => {
+        console.log('SW registration failed: ', registrationError)
+      })
+    })
   }
-  console.log('Service worker not supported in this browser')
 }
 export default swRegister
